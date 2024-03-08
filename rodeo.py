b33ead2471551ca9c49a4b1a42bae3f85c85bcf6
zahedisyed <zahedisyed1@gmail.com>
2024-03-08 23:45:34 +0300

    Upload Starter Code

---
+++ rodeo.py
@@ -0,0 +1,22 @@
+def rename_rodeo_columns (columns):
+    # Replace this and the line below with your code...
+    raise NotImplementedError
+    
+    
+if __name__ == "__main__":
+    # Create a list of example original column names 
+    original = [
+        "Participant Count Data",
+        "Rodeo Clown Count",
+        "Hats Visible",
+        "Cost Data",
+    ]
+    
+    # Get the corrected column names
+    corrected = rename_rodeo_columns (original)
+    
+    # Print a table to compare original and corrected names
+    print("Original                  Corrected")
+    print("--------                  ---------")
+    for o, c in zip(original, corrected):
+        print(f"{o:<25} {c:<25}")
